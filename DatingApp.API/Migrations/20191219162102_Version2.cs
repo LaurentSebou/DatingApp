8cee2beaccd60e8e4d93f15cbba5a4c5b9054f61
Laurent Sebou <sebouhien211@outlook.fr>
2020-01-28 10:20:16 +0000

    Ajoutez des fichiers projet.

---
+++ DatingApp.API/Migrations/20191219162102_Version2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class Version2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Values",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Values",
+                newName: "name");
+        }
+    }
+}
